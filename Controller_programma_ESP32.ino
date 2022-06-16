24f3e6d505fe9ad001ebfda4f177d33080d4d514
rawshaxx <107463677+rawshaxx@users.noreply.github.com>
2022-06-16 13:59:01 +0200

    V3
    Deze versie heeft nu een Snelheids display

--- Controller_programma_ESP32.ino
+++ Controller_programma_ESP32.ino
@@ -21,9 +21,9 @@ const char * pwd = "oboRacit";//dit is het wachtwoord van het gebruikte wifi net
 
 const char * udpAddress = "141.252.29.102"; // het ip adres waar udp berichten naar toe wordt gestuurd 192.168.50.1
 const int udpPort = 8080; // de poort waar udp berichten naar gestuurd worden.
-const int buttonPin1 = 19;// knop 1 Profiel knop
-const int buttonPin2 = 5;//knop 2 Nood knop
-const int buttonPin3 = 17;//knop 3 Variabele knop 
+const int profilePin = 19;// knop 1 Profiel knop
+const int emergencyPin = 5;//knop 2 Nood knop
+const int actionPin = 17;//knop 3 Actie knop 
 int L_JoyStick_X = 35; // Analog Pin  X
 int L_JoyStick_Y = 34; // // Analog Pin  Y
 int L_JoyStick_button = 32; // IO Pin 
@@ -31,6 +31,7 @@ int R_JoyStick_X = 14; // Analog Pin  X
 int R_JoyStick_Y = 27; // // Analog Pin  Y
 int R_JoyStick_button = 12; // IO Pin 
 int profile=0;//integer voor profile switch
+int limiter = 1;
 int i;
 int j=0;
 int LJ_x, LJ_y,RJ_x,RJ_y, button;
@@ -41,7 +42,7 @@ const int DEBOUNCE_DELAY = 10;   // the debounce time; increase if the output fl
 // Variables will change:
 int lastSteadyState = LOW;       // the previous steady state from the input pin
 int lastFlickerableState = LOW;  // the previous flickerable state from the input pin
-int currentState;                // the current reading from the input pin
+int stateProfile;                // the current reading from the input pin
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 
@@ -50,7 +51,7 @@ const int DEBOUNCE_DELAY3 = 40;   // the debounce time; increase if the output f
 // Variables will change:
 int lastSteadyState3 = LOW;       // the previous steady state from the input pin
 int lastFlickerableState3 = LOW;  // the previous flickerable state from the input pin
-int currentState3;                // the current reading from the input pin
+int stateAction;                // the current reading from the input pin
 
 unsigned long lastDebounceTime3 = 0;  // the last time the output pin was toggled
 
@@ -58,14 +59,11 @@ unsigned long lastDebounceTime3 = 0;  // the last time the output pin was toggle
 //create UDP instance
 WiFiUDP udp;
 
-
-
-
 void setup(){
   Serial.begin(115200);               // start de seriele communicatie
-  pinMode(buttonPin1, INPUT_PULLDOWN);
-  pinMode(buttonPin2, INPUT_PULLDOWN);
-  pinMode(buttonPin3, INPUT_PULLDOWN);
+  pinMode(profilePin, INPUT_PULLDOWN);
+  pinMode(emergencyPin, INPUT_PULLDOWN);
+  pinMode(actionPin, INPUT_PULLDOWN);
   pinMode(L_JoyStick_X, INPUT);
   pinMode(L_JoyStick_Y, INPUT);
   pinMode(L_JoyStick_button, INPUT_PULLUP);
@@ -75,55 +73,6 @@ void setup(){
   u8g2.begin();//begin voor OLED scherm
  
 }
-void connect(){
-  //Connect to the WiFi network
-  Serial.println("");// spacer
-  Serial.print("Connecting");
-  WiFi.begin(ssid, pwd);//maak de wifi verbinding.
-  i=0;
-  
-  while ((WiFi.status() != WL_CONNECTED) && (i<20)) {// hier wordt gekeken of er verbinding is met het wifi en dat i nog kleiner is dan 10
-    delay(500);
-    Serial.print(".");
-    i++;
-  }
-  
-  if (WiFi.status() == WL_CONNECTED) {
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  }
-
-}
-
-void u8g2_prepare(void) {   //benodigheden voor het scherm.
-  u8g2.setFont(u8g2_font_6x10_tf);
-  u8g2.setFontRefHeightExtendedText();
-  u8g2.setDrawColor(1);
-  u8g2.setFontPosTop();
-  u8g2.setFontDirection(0);
-}
-
-void showonoled() {
-  u8g2.clearBuffer();
-  u8g2_prepare();
-  
-  u8g2.drawStr(0,36, "Profile:");
-  u8g2.setCursor(70, 36);//selecteer de positie
-  u8g2.print(profile);//display de waarde van het profile
-
-  u8g2.sendBuffer();  
-}
-
-void Startup() {
-  u8g2.clearBuffer();
-  u8g2_prepare();
-  u8g2.drawStr(0,36, "Connecting....");
-  u8g2.sendBuffer();  
-}
-
 
 void loop(){
 
@@ -162,66 +111,78 @@ void loop(){
     sendMessage(RJB);
   }
 
-  currentState = digitalRead(buttonPin1);
-  if (currentState != lastFlickerableState) {
+  stateProfile = digitalRead(profilePin);
+  if (stateProfile != lastFlickerableState) {
     // reset the debouncing timer
     lastDebounceTime = millis();
     // save the the last flickerable state
-    lastFlickerableState = currentState;
+    lastFlickerableState = stateProfile;
   }
   if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (lastSteadyState == HIGH && currentState == LOW){
+    if (lastSteadyState == HIGH && stateProfile == LOW){
       Serial.println("The button is released");
       profile++;
     }
-    else if (lastSteadyState == LOW && currentState == HIGH)
+    else if (lastSteadyState == LOW && stateProfile == HIGH)
       Serial.println("The button is pressed");
 
     // save the the last steady state
-    lastSteadyState = currentState;
+    lastSteadyState = stateProfile;
   
     
     
     if (profile>1){
       profile=0;
     }
-    String Button1 = "{\"MT\":\"PB\", \"p\" :" + String(profile) + "}";           //"{\"MessageType\":\"ProfileButton\", \"Profile\" : profile}";
-    sendMessage(Button1);
+    String profile = "{\"MT\":\"PB\", \"p\" :" + String(profile) + "}";           //"{\"MessageType\":\"ProfileButton\", \"Profile\" : profile}";
+    sendMessage(profile);
     
   }
 
-  if (digitalRead(buttonPin2)){
-    String Button2 = "{\"MT\":\"EB\", \"p\" :" + String(profile) + "}";                 //"{\"MessageType\":\"EmergencyButton\" \"Profile\" : profile}";
-    sendMessage(Button2);
+  if (digitalRead(emergencyPin)){
+    String Stop = "{\"MT\":\"EMERGENCY_BUTTON\", \"p\" :" + String(profile) + "}";                 //"{\"MessageType\":\"EmergencyButton\" \"Profile\" : profile}";
+    sendMessage(Stop);
   }
 
-  currentState3 = digitalRead(buttonPin3);
-  if (currentState3 != lastFlickerableState3) {
+  stateAction = digitalRead(actionPin);
+  if (stateAction != lastFlickerableState3) {
     // reset the debouncing timer
     lastDebounceTime3 = millis();
     // save the the last flickerable state
-    lastFlickerableState3 = currentState3;
+    lastFlickerableState3 = stateAction;
   }
   if ((millis() - lastDebounceTime3) > DEBOUNCE_DELAY) {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
     // if the button state has changed:
-    if (lastSteadyState3 == HIGH && currentState3 == LOW){
+    if (lastSteadyState3 == HIGH && stateAction == LOW){
       Serial.println("The button is released");
-         String Button3 = "{\"MT\":\"VB\", \"p\" :" + String(profile) + "}";                 //"{\"MessageType\":\"VariableButton\" \ "Profile\" : profile}";
-    sendMessage(Button3);
+      if (profile == 0){
+        limiter++;
+      }
     }
-    else if (lastSteadyState3 == LOW && currentState3 == HIGH)
+    else if (lastSteadyState3 == LOW && stateAction == HIGH)
       Serial.println("The button is pressed");
 
     // save the the last steady state
-    lastSteadyState3 = currentState3;
- 
+    lastSteadyState3 = stateAction;
+    
+    if (profile == 0){
+      if (limiter > 3){
+        limiter = 1;
+      }
+      String action = "{\"MT\":\"AB\", \"l\" :" + String(limiter) + "}";                 //"{\"MessageType\":\"ActionButton\" \ "limit\" : limiter}";
+      sendMessage(action);
+    }
+    else{
+      
+    }
+    
   }
    
   u8g2.firstPage();  
@@ -234,6 +195,8 @@ void loop(){
  
 }
 
+
+
 void sendMessage(String message){
   DynamicJsonDocument doc(100);
   deserializeJson(doc, message);
@@ -241,4 +204,57 @@ void sendMessage(String message){
   serializeJson(doc, udp);
   udp.endPacket();
   doc.clear();
+}
+
+void connect(){
+  //Connect to the WiFi network
+  Serial.println("");// spacer
+  Serial.print("Connecting");
+  WiFi.begin(ssid, pwd);//maak de wifi verbinding.
+  i=0;
+  
+  while ((WiFi.status() != WL_CONNECTED) && (i<20)) {// hier wordt gekeken of er verbinding is met het wifi en dat i nog kleiner is dan 10
+    delay(500);
+    Serial.print(".");
+    i++;
+  }
+  
+  if (WiFi.status() == WL_CONNECTED) {
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  }
+
+}
+
+void u8g2_prepare(void) {   //benodigheden voor het scherm.
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+}
+
+void showonoled() {
+  u8g2.clearBuffer();
+  u8g2_prepare();
+  
+  u8g2.drawStr(0,20, "Profile:");
+  u8g2.drawStr(0,40, "Snelheid:");
+  u8g2.setCursor(60, 20);//selecteer de positie
+  u8g2.print(profile);//display de waarde van het profile
+  u8g2.setCursor(60, 40);//selecteer de positie
+  u8g2.print(String(100/limiter) + "%");//display de waarde van het profile
+
+
+  u8g2.sendBuffer();  
+}
+
+void Startup() {
+  u8g2.clearBuffer();
+  u8g2_prepare();
+  u8g2.drawStr(0,36, "Connecting....");
+  u8g2.sendBuffer();  
 }
